3cd836ccfee890a0d69c9304195138c20e203596
Jeremy Fong <jeremy.fong731@gmail.com>
2022-05-27 00:51:01 -0500

    Create dismissible pop-up sheet with item details

--- tracker/ContentView.swift
+++ tracker/ContentView.swift
@@ -45,9 +45,7 @@ struct ContentView: View {
     }
     
     private func toggleShowingWizard() {
-        let _ = print(showingWizard)
         showingWizard = !showingWizard
-        let _ = print(showingWizard)
     }
     
 }
@@ -82,30 +80,45 @@ struct HomeScreenView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.tracking?.estimatedEndDate, ascending: false)],
         animation: .default)
     private var items: FetchedResults<Item>
+    @State var shouldShowSheet: Bool = false
+    @State var itemToShow: Item?
 
     var body: some View {
         List {
             ForEach(items) { item in
-                NavigationLink {
-                    Text(item.icon ?? "🐣")
-                    Text(item.name ?? "New Item")
-                    Text(getItemTypeString(item: item))
-                    Text("Item size is \(item.itemSize?.amount ?? 1) \(item.itemSize?.sizeUnit ?? "L")")
-
-                    if (item.tracking?.estimatedEndDate != nil) {
-                        Text("There are \(numberOfDaysBetween(Date(), and: (item.tracking?.estimatedEndDate)!)) \(item.tracking?.timeUnit ?? "days") remaining")
-                    } else {
-                        Text("You started using this item on \(item.tracking?.startDate ?? Date(), formatter: itemFormatter)")
-                    }
-                
-                } label: {
-                    Text("\(item.name!), 1 day remaining")
+                Button(item.name!) {
+                    itemToShow = item
+                    shouldShowSheet = true
+//                    Text(item.icon ?? "🐣")
+//                    Text(item.name ?? "New Item")
+//                    Text(getItemTypeString(item: item))
+//                    Text("Item size is \(item.itemSize?.amount ?? 1) \(item.itemSize?.sizeUnit ?? "L")")
+//
+//                    if (item.tracking?.estimatedEndDate != nil) {
+//                        Text("There are \(numberOfDaysBetween(Date(), and: (item.tracking?.estimatedEndDate)!)) \(item.tracking?.timeUnit ?? "days") remaining")
+//                    } else {
+//                        Text("You started using this item on \(item.tracking?.startDate ?? Date(), formatter: itemFormatter)")
+//                    }
+//                } label: {
+//                    Text("\(item.name!), 1 day remaining")
+//                }
+//                .onTapGesture {
+//                    itemToShow = item
+//                    shouldShowSheet = true
                 }
             }
             .onDelete(perform: deleteItems)
         }
+        if (itemToShow != nil) {
+            OverviewSheetView(item: itemToShow, onDismiss: dismissSheet)
+        }
     }
     
+    private func dismissSheet() {
+        shouldShowSheet = false
+        itemToShow = nil
+    }
+
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
@@ -155,28 +168,96 @@ struct HomeScreenView: View {
             }
         }
     }
+        
+}
+
+struct OverviewSheetView: View {
+    var item: Item?
+    var onDismiss: () -> Void
+    @State private var offsets = (middle: CGFloat.zero, bottom: CGFloat.zero)
+    @State private var offset: CGFloat = .greatestFiniteMagnitude
+    @State private var lastOffset: CGFloat = .greatestFiniteMagnitude
     
+    var body: some View {
+        if item != nil {
+            GeometryReader { geometry in
+                VStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .foregroundColor(.gray)
+                        .frame(width: 100, height: 10)
+                    Text(item!.name!)
+                        .font(.largeTitle)
+                    Text(item!.icon ?? "🐣")
+                    Text(getItemTypeString(item: item!))
+                    Text("Item size is \(item!.itemSize?.amount ?? 1) \(item!.itemSize?.sizeUnit ?? "L")")
+
+                    if (item!.tracking?.estimatedEndDate != nil) {
+                        Text("There are \(numberOfDaysBetween(Date(), and: (item!.tracking?.estimatedEndDate)!)) \(item!.tracking?.timeUnit ?? "days") remaining")
+                    } else {
+                        Text("You started using this item on \(item!.tracking?.startDate ?? Date(), formatter: itemFormatter)")
+                    }
+                    Spacer()
+                }
+                .padding()
+                .frame(width: geometry.size.width, height: self.offset)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: min(self.offset, 20) ))
+                .animation(Animation.interactiveSpring())
+                .onAppear {
+                    self.offsets = (
+                        middle: geometry.size.height / 2,
+                        bottom: geometry.size.height * 3 / 4
+                    )
+                    self.offset = geometry.size.height * 3 / 4
+                    self.lastOffset = self.offset
+                }
+                .offset(y: self.offset)
+                .gesture(DragGesture(minimumDistance: 5)
+                    .onChanged { v in
+                        let newOffset = self.lastOffset + v.translation.height
+                        if (newOffset > self.offsets.middle) {
+                            self.offset = newOffset
+                        }
+                    }
+                    .onEnded{ v in
+                        if (self.lastOffset == self.offsets.middle) {
+                            if (v.translation.height < 0) {
+                                self.offset = self.offsets.middle
+                            } else {
+                                self.offset = self.offsets.bottom
+                            }
+                        } else {
+                            if (v.translation.height > 0) {
+                                onDismiss()
+                            } else {
+                                self.offset = self.offsets.middle
+                            }
+                        }
+                        self.lastOffset = self.offset
+                    }
+                )
+            }
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+
     private func getItemTypeString(item: Item) -> String {
         let subclassString: String = item.itemType?.itemSubclass?.isEmpty ?? true ? "" : " - \(String(describing: item.itemType?.itemSubclass))"
         
         return "\(String(describing: item.itemType?.itemClass))\(subclassString)"
     }
-    
-    private func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
-        let calendar = Calendar.current
-        let date1 = calendar.startOfDay(for: from)
-        let date2 = calendar.startOfDay(for: to)
-
-        let numberOfDays = calendar.dateComponents([.day], from: date1, to: date2)
-        
-        return numberOfDays.day!
-    }
-
-
 
 }
 
+private func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
+    let calendar = Calendar.current
+    let date1 = calendar.startOfDay(for: from)
+    let date2 = calendar.startOfDay(for: to)
 
+    let numberOfDays = calendar.dateComponents([.day], from: date1, to: date2)
+    
+    return numberOfDays.day!
+}
 
 private let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()
